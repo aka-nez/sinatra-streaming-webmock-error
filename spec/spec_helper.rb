6c31ca1189c30d63e4fa42e26f76648e0877dcec
Viacheslav Nepomniashchikh <vnepomnyashchih@wallarm.com>
2023-01-21 17:42:26 +0700

    Added webmock and reproduced error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'rack/test'
+require 'webmock/rspec'
 require 'byebug'
 require './app.rb'
+
+# Let's imagine, that here is allowed to make request to your local S3 storage
+# like minio or fake-gcs-server
+WebMock.disable_net_connect!(allow: 'https://postman-echo.com')
 
 RSpec.configure do |config|
   config.color = true
